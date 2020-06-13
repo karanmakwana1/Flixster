f37b5ad5e1f770d253d00609d99b587b6861baa1
Karan Makwana <kmakwana1@toromail.csudh.edu>
2020-06-13 00:41:52 -0700

    DONE

---
+++ Flixster/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flixster
+//
+//  Created by Karan Makwana on 12/06/20.
+//  Copyright © 2020 karanmakwana1. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
