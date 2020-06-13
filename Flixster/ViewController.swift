033aec25f3a3699f61e055cebd6874b27a9436e8
Karan Makwana <kmakwana1@toromail.csudh.edu>
2020-06-12 21:28:20 -0700

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Karan Makwana on 12/06/20.
+//  Copyright © 2020 karanmakwana1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
