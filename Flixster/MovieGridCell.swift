9f4802121cff239f1b84dd3ee480cd330d8bb6d5
Karan Makwana <kmakwana1@toromail.csudh.edu>
2020-06-19 23:08:52 -0700

    done flixster part 2

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Karan Makwana on 19/06/20.
+//  Copyright © 2020 karanmakwana1. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
+
